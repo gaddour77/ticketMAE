61b407f31365c3b804560489ad9d1271e89d4d52
gaddour77 <mohamedabdelkader.benfraj@esprit.tn>
2024-06-29 00:36:31 +0100

    first commit

---
+++ src/main/java/tn/esprit/ticketmaeassurrance/TicketMaeAssurranceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.ticketmaeassurrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMaeAssurranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMaeAssurranceApplication.class, args);
+    }
+
+}
